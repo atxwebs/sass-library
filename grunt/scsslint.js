a31a49785e54d62f3b333529b3f8f06185e7e196
Si Taggart <me@simontaggart.com>
2015-01-29 14:15:51 +1100

    Update watch tasks to copy and lint changed files only

--- grunt/scsslint.js
+++ grunt/scsslint.js
@@ -1,11 +1,13 @@
 // Lint our SCSS
 module.exports = {
-    "allFiles": [
-        "src/components/**/*.scss",
-        "src/settings/**/*.scss",
-        "src/tools/**/*.scss",
-        "src/utilities/**/*.scss"
-    ],
+    "all": {
+        "src": [
+            "src/components/**/*.scss",
+            "src/settings/**/*.scss",
+            "src/tools/**/*.scss",
+            "src/utilities/**/*.scss"
+        ]
+    },
     "options": {
        "config": ".scss-lint.yml",
        "force": false
--- grunt/watch.js
+++ grunt/watch.js
@@ -1,11 +1,24 @@
-module.exports = {
-    "sass_files": {
-        "files": [
-            "src/**/*.scss",
-        ],
-        "tasks": [
-            "scsslint",
-            "copy:release"
-        ]
-    }
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-newer');
+
+    // on watch events configure jshint:all to only run on changed file
+    grunt.event.on('watch', function(action, filepath) {
+        grunt.config('scsslint.all.src', filepath);
+    });
+
+    return {
+        "sass_files": {
+            "files": [
+                "src/**/*.scss",
+            ],
+            "tasks": [
+                "scsslint",
+                "newer:copy:release"
+            ],
+            "options": {
+                "spawn": false,
+            }
+        }
+    };
 };
